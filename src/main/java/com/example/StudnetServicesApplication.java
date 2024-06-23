6269b3cbcd56b05b6627944a1cb604e6c7db0a87
malli031998 <vvdmallikarjunareddy.com>
2024-06-23 11:24:10 +0530

    my first commit in student service

---
+++ src/main/java/com/example/StudnetServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudnetServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudnetServicesApplication.class, args);
+	}
+
+}
